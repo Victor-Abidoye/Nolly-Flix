0cd92d8267481d833e5f7b70050ce3d7023517c4
Victor <abidoye8224@gmail.com>
2022-05-21 22:12:24 +0100

    filter by search

--- index.js
+++ index.js
@@ -28,32 +28,44 @@ const newSlide = () => {
 
 const movies = [
     {
+        id: 1,
         name: "The Redemption",
         likes: 5,
         src: "./assets/img/p9649633_p_v8_ai.jpg",
         Genre: ["action"]
     },
     {
+        id: 2,
         name: "Two",
         likes: 4,
         src: "./assets/img/p9649633_p_v8_ai.jpg",
         Genre: ["action"]
     },
     {
+        id: 3,
         name: "loas",
         likes: 8,
         src: "./assets/img/p9649633_p_v8_ai.jpg",
         Genre: ["action"]
     },
     {
+        id: 4,
         name: "male",
         likes: 3,
         src: "./assets/img/p9649633_p_v8_ai.jpg",
         Genre: ["action"]
     },
     {
+        id: 5,
         name: "dkldj",
         likes: 5,
+        src: "./assets/img/p9649633_p_v8_ai.jpg",
+        Genre: ["action"]
+    },
+    {
+        id: 6,
+        name: "dkldj",
+        likes: 6,
         src: "./assets/img/p9649633_p_v8_ai.jpg",
         Genre: ["action"]
     }
@@ -99,5 +111,42 @@ function display (arr) {
 }
 
 
-display(storeMovies)
+let newArr = []
+function orders (arr) {
+    if (!arr.length) {
+        return
+    }
+
+
+    let highest = -1
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].likes > highest) {
+            highest = arr[i].likes
+        }
+    }
+
+    console.log(highest);
+    let high = arr.findIndex(movie => {
+        console.log(movie);
+        return movie.likes == highest
+    })
+    console.log(high);
+
+    newArr.push(arr[high])
+     arr.splice(high, 1)
+    console.log(arr)
+
+    orders(arr)
+}
+
+orders(storeMovies)
+console.log(newArr)
+
+
+
+
+
+
+
+display(newArr)
 newSlide()
