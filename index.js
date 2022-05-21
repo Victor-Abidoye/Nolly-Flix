55ecd6ceba6def717ac91da61baded7db3235f3e
Victor <abidoye8224@gmail.com>
2022-05-21 21:01:26 +0100

    Added movie card to dashboard

--- index.js
+++ index.js
@@ -26,9 +26,78 @@ const newSlide = () => {
     })
 }
 
+const movies = [
+    {
+        name: "The Redemption",
+        likes: 5,
+        src: "./assets/img/p9649633_p_v8_ai.jpg",
+        Genre: ["action"]
+    },
+    {
+        name: "Two",
+        likes: 4,
+        src: "./assets/img/p9649633_p_v8_ai.jpg",
+        Genre: ["action"]
+    },
+    {
+        name: "loas",
+        likes: 8,
+        src: "./assets/img/p9649633_p_v8_ai.jpg",
+        Genre: ["action"]
+    },
+    {
+        name: "male",
+        likes: 3,
+        src: "./assets/img/p9649633_p_v8_ai.jpg",
+        Genre: ["action"]
+    },
+    {
+        name: "dkldj",
+        likes: 5,
+        src: "./assets/img/p9649633_p_v8_ai.jpg",
+        Genre: ["action"]
+    }
+]
+
+//Grab DOM Element
+let movie_container = document.querySelector('#movie_container')
+
+let storeMovies
+// check localStorage for saved movies data
+storeMovies = JSON.parse(localStorage.getItem("movies"))
+
+if (!storeMovies) {
+    //load to localStorage
+    localStorage.setItem("movies", JSON.stringify(movies))
+
+}
+storeMovies = JSON.parse(localStorage.getItem("movies"))
 
 
+function display (arr) {
+    for (let i = 0; i < arr.length; i++){
+        movie_container.innerHTML += `<div class="shadow-md items-center max-w-xs w-64 h-80 bg-little-100 dark:bg-prudent-100 rounded-md">
+                <div class="w-full h-1/2">
+                    <img src="./assets/img/p9649633_p_v8_ai.jpg" alt="" class="w-full h-full object-cover rounded-t-md" />
+                </div>
+                <div class="px-5 text-black">
+                    <h2 class="font-bold text-center text-lg py-3">${arr[i].name}</h2>
+                    <p class="font-medium text-sm py-1">
+                        Genre:
+                        <span class="font-normal">
+                    </p>
+                    <p class="font-medium text-sm py-1">
+                        Price: <span class="font-normal">N1000</span>
+                    </p>
+                    <p class="font-medium text-sm py-1 text-right">
+                    <i class="fa-solid fa-magnifying-glass mr-5"> aldsk</i><span>${arr[i].likes}</span>
+                    </p>
+
+                </div>
+            </div>`
+    }
+}
 
 
-
+display(storeMovies)
 newSlide()
